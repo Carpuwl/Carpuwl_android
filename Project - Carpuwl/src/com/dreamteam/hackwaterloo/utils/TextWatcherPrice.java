1269cfb54123166cb7c438fd6d22bcf9faf21f36
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2014-04-05 14:32:22 -0400

    Fix for price field

--- Project - Carpuwl/src/com/dreamteam/hackwaterloo/utils/TextWatcherPrice.java
+++ Project - Carpuwl/src/com/dreamteam/hackwaterloo/utils/TextWatcherPrice.java
@@ -1,6 +1,8 @@
 package com.dreamteam.hackwaterloo.utils;
 
 import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -8,35 +10,45 @@ import android.util.Log;
 import android.widget.EditText;
 
 public class TextWatcherPrice implements TextWatcher {
-
+    
+    private static final NumberFormat numberFormat = NumberFormat.getCurrencyInstance(Locale.CANADA);
+    private static final DecimalFormat decimalFormat = new DecimalFormat("##.##");
+    
     private EditText mEditText;
-    private String current;
+    private String mContentString;
 
     public TextWatcherPrice(EditText editText) {
         mEditText = editText;
-        current = "";
+        mContentString = "";
     }
 
     @Override
     public void afterTextChanged(Editable s) {
-        // TODO Auto-generated method stub
+        // No implementation
     }
 
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        // TODO Auto-generated method stub
+        // No implementation
     }
 
+    // TODO: fix this 
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
-        if (!s.toString().equals(current)) {
+        if (!s.toString().equals(mContentString)) {
             mEditText.removeTextChangedListener(this);
+
+            String cleanString = s.toString().replace(".", "");
+
+            double parsed = Double.parseDouble(cleanString);
+            String formatted = numberFormat.format(parsed / 100);
             
-            Log.d("ryan", "edittext is : " + mEditText.toString());
-            String formatted = new DecimalFormat("##.##").format(Double.parseDouble(s.toString()));
+            formatted = formatted.replace("$", "");
+            Log.d("ryan", "formatted: " + formatted);
+            mContentString = formatted;
             mEditText.setText(formatted);
             mEditText.setSelection(formatted.length());
-            
+
             mEditText.addTextChangedListener(this);
         }
     }
