64559cdab90913a3efd7e9ed4eb8a81960a3d324
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2014-04-06 20:20:24 -0400

    Enable all ui widgets in the filter drawer

--- Project - Carpuwl/src/com/dreamteam/hackwaterloo/fragments/FragmentFilter.java
+++ Project - Carpuwl/src/com/dreamteam/hackwaterloo/fragments/FragmentFilter.java
@@ -2,20 +2,147 @@ package com.dreamteam.hackwaterloo.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.dreamteam.carpuwl.R;
+import com.dreamteam.hackwaterloo.Constants.Defaults;
+import com.dreamteam.hackwaterloo.utils.DateTimePickerHelper;
+import com.dreamteam.hackwaterloo.utils.DateTimePickerHelper.OnDateTimeSelectedListener;
+import com.dreamteam.hackwaterloo.utils.TextWatcherPrice;
+import com.dreamteam.hackwaterloo.utils.Utils;
 
-public class FragmentFilter extends SherlockFragment {
-    
+public class FragmentFilter extends SherlockFragment implements OnClickListener {
+
     public static final String FRAGMENT_TAG = FragmentFilter.class.getSimpleName();
+    
+    private DateTimePickerHelper mDateTimePickerHelper;
+
+    private Spinner mSpinnerDepart;
+    private Spinner mSpinnerArrive;
+    private Spinner mSpinnerWhen;
+    private EditText mEditPrice;
+    private SeekBar mSeekBarSeats;
+    private Button mButtonWhen;
+    private Button mButtonApply;
+    private TextView mTextSeats;
+    private TextView mTextWhen;
+
+    private CheckBox mCheckBoxSpinnerDepart;
+    private CheckBox mCheckBoxSpinnerArrive;
+    private CheckBox mCheckBoxSpinnerWhen;
+    private CheckBox mCheckBoxEditTextPrice;
+    private CheckBox mCheckBoxSeekBarSeats;
+    private CheckBox mCheckBoxButtonWhen;
+    private CheckBox mCheckBoxButtonApply;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.filter_view, null, false);
+        View rootView = inflater.inflate(R.layout.fragment_filter, null, false);
+
+        mSpinnerDepart = (Spinner) rootView.findViewById(R.id.filter_spinner_depart_location);
+        mSpinnerArrive = (Spinner) rootView.findViewById(R.id.filter_spinner_arrival_location);
+        mSpinnerWhen = (Spinner) rootView.findViewById(R.id.filter_spinner_when);
+        mEditPrice = (EditText) rootView.findViewById(R.id.filter_edittext_price);
+        mSeekBarSeats = (SeekBar) rootView.findViewById(R.id.filter_seekbar_seats);
+        mButtonWhen = (Button) rootView.findViewById(R.id.filter_button_when);
+        mButtonApply = (Button) rootView.findViewById(R.id.filter_button_apply);
+        mTextSeats = (TextView) rootView.findViewById(R.id.filter_text_content_seats);
+        mTextWhen = (TextView) rootView.findViewById(R.id.filter_text_content_when);
+
+        // Set up the spinners
+        ArrayAdapter<CharSequence> spinnerAdapterWhen = ArrayAdapter.createFromResource(
+                getActivity(), R.array.filter_when_type,
+                android.R.layout.simple_spinner_dropdown_item);
+        mSpinnerWhen.setAdapter(spinnerAdapterWhen);
+
+        ArrayAdapter<CharSequence> spinnerAdapterCities = ArrayAdapter.createFromResource(
+                getActivity(), R.array.cities, android.R.layout.simple_spinner_dropdown_item);
+        mSpinnerDepart.setAdapter(spinnerAdapterCities);
+        mSpinnerArrive.setAdapter(spinnerAdapterCities);
+
+        mEditPrice.addTextChangedListener(new TextWatcherPrice(mEditPrice));
+
+        mSeekBarSeats.setOnSeekBarChangeListener(new SeekBarInDrawerListener());
+        mSeekBarSeats.setOnTouchListener(new SeekBarTouchOverride());
+        mSeekBarSeats.setProgress(Defaults.MINIMUM_SEATS);
         
+        mButtonWhen.setOnClickListener(this);
+
         return rootView;
+    }
+
+    // OnTouchListener implementation in order to stop the NavigationDrawer from
+    // hijacking touch events that should be registered with the seekbar.
+    private class SeekBarTouchOverride implements OnTouchListener {
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    // Disallow Drawer to intercept touch events.
+                    v.getParent().requestDisallowInterceptTouchEvent(true);
+                    break;
+
+                case MotionEvent.ACTION_UP:
+                    // Allow Drawer to intercept touch events.
+                    v.getParent().requestDisallowInterceptTouchEvent(false);
+                    break;
+            }
+
+            // Handle seekbar touch events.
+            v.onTouchEvent(event);
+            return true;
+        }
+    }
+
+    private class SeekBarInDrawerListener implements OnSeekBarChangeListener {
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            mTextSeats.setText(String.valueOf(progress));
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            if (mTextSeats.getText().toString().equals("0")) {
+                seekBar.setProgress(Defaults.MINIMUM_SEATS);
+            }
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.filter_button_when:
+                mDateTimePickerHelper = new DateTimePickerHelper(getActivity()
+                        .getSupportFragmentManager());
+                mDateTimePickerHelper.setOnDateTimeSelectedListener(new DateTimePickerListener());
+                mDateTimePickerHelper.show();
+        }
+    }
+    
+    private class DateTimePickerListener implements OnDateTimeSelectedListener {
+        @Override
+        public void onDateTimeSelected(long timeInMillis) {
+            mTextWhen.setText(Utils.multiCaseDateFormat(timeInMillis));
+        }
     }
 }
--- Project - Carpuwl/src/com/dreamteam/hackwaterloo/fragments/FragmentFindARide.java
+++ Project - Carpuwl/src/com/dreamteam/hackwaterloo/fragments/FragmentFindARide.java
@@ -2,26 +2,16 @@ package com.dreamteam.hackwaterloo.fragments;
 
 import java.util.Arrays;
 
-import android.app.Activity;
-import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.Spinner;
 
-import com.actionbarsherlock.app.SherlockDialogFragment;
 import com.actionbarsherlock.app.SherlockFragment;
 import com.dreamteam.carpuwl.R;
-import com.dreamteam.hackwaterloo.activities.ActivityMain;
 import com.dreamteam.hackwaterloo.adapters.Feed;
 import com.dreamteam.hackwaterloo.adapters.Feed.Event;
 import com.dreamteam.hackwaterloo.adapters.FeedAdapter;
@@ -29,22 +19,13 @@ import com.dreamteam.hackwaterloo.utils.CrossFadeViewSwitcher;
 import com.dreamteam.hackwaterloo.utils.server.BaseTask.OnPostExecuteListener;
 import com.dreamteam.hackwaterloo.utils.server.GetEventsTask;
 
-public class FragmentFindARide extends SherlockFragment implements OnClickListener {
+public class FragmentFindARide extends SherlockFragment {
 
     public static final String FRAGMENT_TAG = FragmentFindARide.class.getSimpleName();
 
-    // Ui Widgets
     private ProgressBar mProgressBar;
     private ListView mListView;
     private FeedAdapter mListAdapter;
-    private Button mFilterButton;
-    private FragmentManager mFragmentManager;
-
-    @Override
-    public void onAttach(Activity activity) {
-        mFragmentManager = ((ActivityMain) activity).getSupportFragmentManager();
-        super.onAttach(activity);
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -52,13 +33,7 @@ public class FragmentFindARide extends SherlockFragment implements OnClickListen
         rootView = inflater.inflate(R.layout.fragment_find_a_ride, container, false);
 
         mProgressBar = (ProgressBar) rootView.findViewById(R.id.find_ride_progress_bar);
-        mFilterButton = (Button) rootView.findViewById(R.id.find_ride_button_filter);
         mListView = (ListView) rootView.findViewById(R.id.find_ride_list_view);
-        
-        if (mFilterButton == null) {
-            Log.e("ryan", "filter button is null!!!!");
-        }
-        mFilterButton.setOnClickListener(this);
         
         getEvents();
         
@@ -79,44 +54,6 @@ public class FragmentFindARide extends SherlockFragment implements OnClickListen
             }
         });
         getEventsTask.executeParallel();
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.find_ride_button_filter:
-                DialogFilter dialogFilter = (DialogFilter) mFragmentManager.findFragmentByTag(DialogFilter.FRAGMENT_TAG);
-                if (dialogFilter == null) {
-                    dialogFilter = new DialogFilter();
-                }
-                dialogFilter.show(mFragmentManager, DialogFilter.FRAGMENT_TAG);
-        }
-    }
-
-    public static class DialogFilter extends SherlockDialogFragment {
-
-        public static final String FRAGMENT_TAG = DialogFilter.class.getSimpleName();
-
-        private Spinner mSpinnerStart;
-        
-        private String[] mCities;
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            Dialog dialog = new Dialog(getActivity());
-            dialog.requestWindowFeature(STYLE_NO_TITLE);
-            dialog.setContentView(R.layout.dialog_filter);
-            dialog.setCancelable(true);
-            dialog.setTitle(R.string.dialog_filter_title);
-
-            mSpinnerStart = (Spinner) dialog.findViewById(R.id.dialog_filter_spinner_startpoint);
-            
-            mCities = getActivity().getResources().getStringArray(R.array.cities);
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, mCities);
-            mSpinnerStart.setAdapter(adapter);
-            
-            return dialog;
-        }
     }
 
     @Override
